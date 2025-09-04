40d79e84bf7165dcc9b73fd4a58a23858cd748f5
Juvin Omondi <juvin.omondi@gmail.com>
2025-09-04 16:36:42 +0300

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/weather_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/data/data_provider/weather_data_provider.dart';
+import 'package:weather_app/presentation/screens/weather_screen.dart';
+
+import 'bloc/weather_bloc.dart';
+import 'data/repository/weather_repository.dart';
+
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp ({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(useMaterial3: true),
-      home:  const WeatherScreen(),
+    return RepositoryProvider(
+      create: (context) => WeatherRepository(
+        WeatherDataProvider(),
+      ),
+      child: BlocProvider(
+        create: (context) => WeatherBloc(context.read<WeatherRepository>(),
+        ),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData.dark(useMaterial3: true),
+          home: const WeatherScreen(),
+        ),
+      ),
     );
   }
 }
